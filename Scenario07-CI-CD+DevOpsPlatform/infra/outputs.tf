6596f993b8a6c59b8d8d6c967bd58a3e82a34ff0
Dragan Z. <95223276+student-asss@users.noreply.github.com>
2026-03-01 11:25:51 +0100

    Update outputs.tf

--- Scenario07-CI-CD+DevOpsPlatform/infra/outputs.tf
+++ Scenario07-CI-CD+DevOpsPlatform/infra/outputs.tf
@@ -1,1 +1,12 @@
+output "log_analytics_workspace_id" {
+  value = module.loganalytics.workspace_id
+}
+
+output "app_service_name" {
+  value = module.appservice.app_name
+}
+
+output "key_vault_name" {
+  value = module.keyvault.kv_name
+}
 
