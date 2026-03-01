44f54bf3c93c829e8e9fda6706816c0547e2b959
Dragan Z. <95223276+student-asss@users.noreply.github.com>
2026-03-01 11:27:11 +0100

    Update main.tf

--- Scenario07-CI-CD+DevOpsPlatform/infra/modules/loganalytics/main.tf
+++ Scenario07-CI-CD+DevOpsPlatform/infra/modules/loganalytics/main.tf
@@ -1,1 +1,12 @@
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "law-s07-monitoring"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+output "workspace_id" {
+  value = azurerm_log_analytics_workspace.law.id
+}
 
