87753dff27f9ae1ca0eb6a6054499a9930bd5416
Dragan Z. <95223276+student-asss@users.noreply.github.com>
2026-03-03 23:16:09 +0100

    Add files via upload

--- Scenario03-API-ScaleSet/terraform/modules/loadbalancer/main.tf
+++ Scenario03-API-ScaleSet/terraform/modules/loadbalancer/main.tf
@@ -1,1 +1,54 @@
+resource "azurerm_monitor_autoscale_setting" "autoscale" {
+  name                = "autoscale-vmss-api"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  target_resource_id  = var.vmss_id
 
+  profile {
+    name = "default"
+
+    capacity {
+      minimum = "2"
+      maximum = "5"
+      default = "2"
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = var.vmss_id
+        operator           = "GreaterThan"
+        statistic          = "Average"
+        threshold          = 70
+        time_grain         = "PT1M"
+        time_window        = "PT5M"
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = var.vmss_id
+        operator           = "LessThan"
+        statistic          = "Average"
+        threshold          = 30
+        time_grain         = "PT1M"
+        time_window        = "PT5M"
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+  }
+}
---
+++ Scenario03-API-ScaleSet/terraform/modules/loadbalancer/outputs.tf
@@ -0,0 +1,3 @@
+output "autoscale_id" {
+  value = azurerm_monitor_autoscale_setting.autoscale.id
+}
